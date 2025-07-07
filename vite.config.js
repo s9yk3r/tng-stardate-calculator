f6129a139589cdf28abde2dc9188039cc188e067
Alessandro Lombi <spyker@live.it>
2025-07-07 14:39:31 +0200

    chore(deploy): build the dist package and update some configurations

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,78 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { obfuscator } from 'rollup-obfuscator'
+
+/**
+ * Obfuscator Config
+ *
+ * @see {@link https://github.com/javascript-obfuscator/javascript-obfuscator#preset-options}
+ */
+const obfuscatorConfig = Object.assign(
+    // Insert javascript-obfuscator's default preset-options
+    {
+        compact: true,
+        controlFlowFlattening: false,
+        deadCodeInjection: false,
+        debugProtection: false,
+        debugProtectionInterval: 0,
+        disableConsoleOutput: false,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        numbersToExpressions: false,
+        renameGlobals: false,
+        selfDefending: false,
+        simplify: true,
+        splitStrings: false,
+        stringArray: true,
+        stringArrayCallsTransform: false,
+        stringArrayCallsTransformThreshold: 0.5,
+        stringArrayEncoding: [],
+        stringArrayIndexShift: true,
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+        stringArrayWrappersCount: 1,
+        stringArrayWrappersChainedCalls: true,
+        stringArrayWrappersParametersMaxCount: 2,
+        stringArrayWrappersType: 'variable',
+        stringArrayThreshold: 0.75,
+        unicodeEscapeSequence: false
+    },
+    // Insert own setting.
+    {
+        stringArray: false // fix for vue inline stylesheet
+    }
+)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    obfuscator({
+      global: true,
+      exclude: ['./node_modules/**/*'],
+      ...obfuscatorConfig
+    })
   ],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    minify: 'esbuild',
+    cssMinify: 'lightningcss',
+    outDir: 'dist',
+    assetsDir: '/assets',
+    emptyOutDir: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
