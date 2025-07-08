5cd34a91ecd88f45e454eef111f06a88f8696110
Alessandro Lombi <spyker@live.it>
2025-07-08 13:41:09 +0200

    chore(deploy): update build files

--- vite.config.js
+++ vite.config.js
@@ -66,7 +66,7 @@ export default defineConfig({
   build: {
     minify: 'esbuild',
     cssMinify: 'lightningcss',
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: '/assets',
     emptyOutDir: false,
     rollupOptions: {
